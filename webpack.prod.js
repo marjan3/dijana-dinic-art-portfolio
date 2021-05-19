31f0fcdc7695e177d72908cf4c4c8f8ea3ff834c
Tanevski3 <marjantanevski@gmail.com>
2021-05-19 20:55:02 +0200

    fix webpack prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,57 +2,60 @@ const COMMON = require("./webpack.common.js");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const ImageminPlugin = require("imagemin-webpack-plugin").default;
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { merge } = require('webpack-merge');
+const TerserPlugin = require("terser-webpack-plugin");
+const { merge } = require("webpack-merge");
 
-
-module.exports = merge (COMMON, {
-    mode: "production",
-    output: {
-        // filename: '[name].[contenthash].bundle.js',
-        filename = "js/app.js"
-    },
-    module: {
-        rules: [
-            {
-                test: [/\.js$/],
-                exclude: [/node_modules/],
-                loader: "babel-loader",
-                options: { presets: ["env"] },
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-          filename: "[name].[hash].css",
-          chunkFilename: "[id].[hash].css",
-        }),
-        new ImageminPlugin({
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          optipng: { optimizationLevel: 3 },
-          jpegtran: { progressive: true },
-          gifsicle: { optimizationLevel: 1 },
-          svgo: {},
-        }),
+module.exports = merge(COMMON, {
+  mode: "production",
+  output: {
+    publicPath: "./",
+    // filename: '[name].[contenthash].bundle.js',
+    filename: "[name].js",
+    chunkFilename: "[id].[chunkhash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.js$/],
+        exclude: [/node_modules/],
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-env"] },
+      },
     ],
-    // https://webpack.js.org/configuration/optimization/
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserPlugin({
-          cache: true,
-          parallel: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+      chunkFilename: "[id].[hash].css",
+    }),
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      optipng: { optimizationLevel: 3 },
+      jpegtran: { progressive: true },
+      gifsicle: { optimizationLevel: 1 },
+      svgo: {},
+    }),
+  ],
+  // https://webpack.js.org/configuration/optimization/
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
           sourceMap: true,
-        }),
-        new CssMinimizerPlugin(),
-      ],
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            test: /[\\/]node_modules[\\/]/,
-            name: "vendors",
-            chunks: "all",
-          },
+        },
+      }),
+      new CssMinimizerPlugin(),
+    ],
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
         },
       },
-    }
+    },
+  },
 });
